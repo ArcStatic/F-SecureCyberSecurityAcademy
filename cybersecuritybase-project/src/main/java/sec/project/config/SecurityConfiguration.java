21b2e5f58a003ee3688af832b199db408ac56470
ArcStatic <eband42@gmail.com>
2017-03-05 22:06:49 +0000

    Course Project 1 should now be accessible

---
+++ cybersecuritybase-project/src/main/java/sec/project/config/SecurityConfiguration.java
@@ -0,0 +1,44 @@
+package sec.project.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // no real security at the moment
+        http.authorizeRequests()
+                .anyRequest().permitAll();
+        
+        
+        //Flaw #1: site is now wide-open to CSRF-attacks!
+        //Any other site can create a POST request to this app now that _csrf attribute is no longer added
+        //OWASP A2 - Broken Authentication and Session Management
+        http
+	.csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
