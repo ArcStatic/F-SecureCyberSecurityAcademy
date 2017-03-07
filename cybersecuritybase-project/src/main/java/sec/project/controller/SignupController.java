7567ba713d0931c89352d533e9e272a3c30b7356
ArcStatic <eband42@gmail.com>
2017-03-07 02:52:40 +0000

    Trying to get persistence working but <root> element in xml seems to be casuing problems. Error whenever accessing loginpage: 'Unrecognised persistence.xml version []'

--- cybersecuritybase-project/src/main/java/sec/project/controller/SignupController.java
+++ cybersecuritybase-project/src/main/java/sec/project/controller/SignupController.java
@@ -1,12 +1,8 @@
 package sec.project.controller;
 
-import javax.crypto.KeyGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.crypto.encrypt.TextEncryptor;
-import org.springframework.security.crypto.keygen.KeyGenerators;
-import org.springframework.security.crypto.keygen.StringKeyGenerator;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +15,7 @@ public class SignupController {
     @Autowired
     private SignupRepository signupRepository;
 
-    @RequestMapping("*")
+    @RequestMapping("/")
     public String defaultMapping() {
         return "redirect:/form";
     }
@@ -30,11 +26,18 @@ public class SignupController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String address, @RequestParam String dateOfBirth, @RequestParam String password) {
-        //Flaw #2:
-        
+    public String submitForm(Model model, @RequestParam String name, @RequestParam String address, @RequestParam String dateOfBirth, @RequestParam String password) {
+        //Flaw #2: personal details and password are all sent through POST as plaintext
+        //OWASP A6: Sensitive data exposure
         signupRepository.save(new Signup(name, address, dateOfBirth, password));
-        return "done";
+        model.addAttribute("userName", name);
+        model.addAttribute("userAddress", address);
+        model.addAttribute("userDateOfBirth", dateOfBirth);
+        model.addAttribute("userPassword", password);
+        //Flaw #2: personal details and password are all sent through POST as plaintext
+        //OWASP A6: Sensitive data exposure
+        String loginurl = "redirect:/loginpage?name=" + name;
+        return loginurl;
     }
 
 }
--- cybersecuritybase-project/src/main/java/sec/project/repository/SignupRepository.java
+++ cybersecuritybase-project/src/main/java/sec/project/repository/SignupRepository.java
@@ -1,8 +1,14 @@
 package sec.project.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
 import sec.project.domain.Signup;
 
 public interface SignupRepository extends JpaRepository<Signup, Long> {
 
+    //Signup findByName(String name);
+    
+    List<Signup> findByName(String name);
+    
 }
